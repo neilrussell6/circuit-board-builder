7576b8ecd9a1db658403a4ecb9f5121a459224a8
Matthew Smith <slopboy@gmail.com>
2021-03-03 18:53:42 +0200

    feat(ui-react): chip truth table

--- packages/ui-react/src/modules/Chip/ChipDetail/ChipDetail.jsx
+++ packages/ui-react/src/modules/Chip/ChipDetail/ChipDetail.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import styles from './ChipDetail.module.css'
+import { ChipTruthTable } from '../ChipTruthTable/ChipTruthTable'
 
 export const ChipDetail = ({ chip }) => {
   const { id, name, description, truthTable, inputs, outputs } = chip
@@ -8,6 +9,12 @@ export const ChipDetail = ({ chip }) => {
     <div>
       <div className={styles.header}>{name}</div>
       <div className={styles.description}>{description}</div>
+
+      {(truthTable.length > 0) &&
+        <div className={styles.truthTable}>
+          <ChipTruthTable truthTable={truthTable} />
+        </div>
+      }
     </div>
   )
 }
