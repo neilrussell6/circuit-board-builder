669466aefc786ae74226e60633cd93ef4544eca9
Matthew Smith <slopboy@gmail.com>
2021-03-22 20:57:52 +0200

    feat: chip circuit boards

--- packages/ui-react/src/modules/Chip/constants.js
+++ packages/ui-react/src/modules/Chip/constants.js
@@ -1,5 +1,12 @@
 import { lib } from '@nr6/nand2tetris-logic-gates'
 
+// TODO: use the NODE_TYPES in CircuitBoard constants
+const NODE_TYPE = {
+  INPUT: 'input',
+  CHIP: 'chip',
+  OUTPUT: 'output',
+}
+
 export const DEFAULT_BLANK_CHIP = {
   id: 0,
   name: '',
@@ -9,8 +16,322 @@ export const DEFAULT_BLANK_CHIP = {
   outputs: [],
 }
 
+export const CHIP_IDS = {
+  ID: 1,
+  NAND: 2,
+  NOT: 3,
+  AND: 4,
+  OR: 5,
+  MUX: 6,
+  DMUX: 7,
+  DEMO_1: 8,
+}
+
+export const CHIP_CIRCUIT_BOARDS = {
+  ID: {
+    start: ['0'],
+    end: ['2'],
+    nodes: {
+      '0': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.INPUT,
+        graphAL: [['1', 0, 0]],
+        f: lib.VALUE (true),
+      },
+      '1': {
+        chipId: CHIP_IDS.ID,
+        label: 'ID',
+        type: NODE_TYPE.CHIP,
+        graphAL: [['2', 0, 0]],
+        f: lib.ID,
+      },
+      '2': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.OUTPUT,
+        graphAL: [],
+        f: lib.ID,
+      },
+    },
+  },
+  NOT: {
+    start: ['0'],
+    end: ['2'],
+    nodes: {
+      '0': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.INPUT,
+        graphAL: [['1', 0, 0]],
+        f: lib.VALUE (true),
+      },
+      '1': {
+        chipId: CHIP_IDS.NOT,
+        label: 'NOT',
+        type: NODE_TYPE.CHIP,
+        graphAL: [['2', 0, 0]],
+        f: lib.NOT,
+      },
+      '2': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.OUTPUT,
+        graphAL: [],
+        f: lib.ID,
+      },
+    },
+  },
+  AND: {
+    start: ['0', '1'],
+    end: ['3'],
+    nodes: {
+      '0': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.INPUT,
+        graphAL: [['2', 0, 0]],
+        f: lib.VALUE (true),
+      },
+      '1': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.INPUT,
+        graphAL: [['2', 0, 1]],
+        f: lib.VALUE (false),
+      },
+      '2': {
+        chipId: CHIP_IDS.AND,
+        label: 'AND',
+        type: NODE_TYPE.CHIP,
+        graphAL: [['3', 0, 0]],
+        f: lib.AND,
+      },
+      '3': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.OUTPUT,
+        graphAL: [],
+        f: lib.ID,
+      },
+    },
+  },
+  NAND: {
+    start: ['0', '1'],
+    end: ['3'],
+    nodes: {
+      '0': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.INPUT,
+        graphAL: [['2', 0, 0]],
+        f: lib.VALUE (true),
+      },
+      '1': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.INPUT,
+        graphAL: [['2', 0, 1]],
+        f: lib.VALUE (false),
+      },
+      '2': {
+        chipId: CHIP_IDS.NAND,
+        label: 'NAND',
+        type: NODE_TYPE.CHIP,
+        graphAL: [['3', 0, 0]],
+        f: lib.NAND,
+      },
+      '3': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.OUTPUT,
+        graphAL: [],
+        f: lib.ID,
+      },
+    },
+  },
+  OR: {
+    start: ['0', '1'],
+    end: ['3'],
+    nodes: {
+      '0': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.INPUT,
+        graphAL: [['2', 0, 0]],
+        f: lib.VALUE (true),
+      },
+      '1': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.INPUT,
+        graphAL: [['2', 0, 1]],
+        f: lib.VALUE (false),
+      },
+      '2': {
+        chipId: CHIP_IDS.OR,
+        label: 'OR',
+        type: NODE_TYPE.CHIP,
+        graphAL: [['3', 0, 0]],
+        f: lib.OR,
+      },
+      '3': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.OUTPUT,
+        graphAL: [],
+        f: lib.ID,
+      },
+    },
+  },
+  MUX: {
+    start: ['0', '1', '2'],
+    end: ['4'],
+    nodes: {
+      '0': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.INPUT,
+        graphAL: [['3', 0, 0]],
+        f: lib.VALUE (true),
+      },
+      '1': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.INPUT,
+        graphAL: [['3', 0, 1]],
+        f: lib.VALUE (false),
+      },
+      '2': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.INPUT,
+        graphAL: [['3', 0, 2]],
+        f: lib.VALUE (false),
+      },
+      '3': {
+        chipId: CHIP_IDS.MUX,
+        label: 'MUX',
+        type: NODE_TYPE.CHIP,
+        graphAL: [['4', 0, 0]],
+        f: lib.MUX,
+      },
+      '4': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.OUTPUT,
+        graphAL: [],
+        f: lib.ID,
+      },
+    },
+  },
+  DMUX: {
+    start: ['0', '1'],
+    end: ['3', '4'],
+    nodes: {
+      '0': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.INPUT,
+        graphAL: [['2', 0, 0]],
+        f: lib.VALUE (true),
+      },
+      '1': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.INPUT,
+        graphAL: [['2', 0, 1]],
+        f: lib.VALUE (false),
+      },
+      '2': {
+        chipId: CHIP_IDS.DMUX,
+        label: 'DMUX',
+        type: NODE_TYPE.CHIP,
+        graphAL: [['3', 0, 0], ['4', 0, 0]],
+        f: lib.DMUX,
+      },
+      '3': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.OUTPUT,
+        graphAL: [],
+        f: lib.ID,
+      },
+      '4': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.OUTPUT,
+        graphAL: [],
+        f: lib.ID,
+      },
+    },
+  },
+  DEMO_1: {
+    start: ['0', '1', '3'],
+    end: ['6', '7'],
+    nodes: {
+      '0': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.INPUT,
+        graphAL: [['2', 0, 0]],
+        f: lib.VALUE (true),
+      },
+      '1': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.INPUT,
+        graphAL: [['2', 0, 1]],
+        f: lib.VALUE (false),
+      },
+      '2': {
+        chipId: CHIP_IDS.AND,
+        label: 'AND',
+        type: NODE_TYPE.CHIP,
+        graphAL: [['5', 0, 0]],
+        f: lib.AND,
+      },
+      '3': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.INPUT,
+        graphAL: [['4', 0, 0]],
+        f: lib.VALUE (true),
+      },
+      '4': {
+        chipId: CHIP_IDS.NOT,
+        label: 'NOT',
+        type: NODE_TYPE.CHIP,
+        graphAL: [['5', 0, 1]],
+        f: lib.NOT,
+      },
+      '5': {
+        chipId: CHIP_IDS.OR,
+        label: 'OR',
+        type: NODE_TYPE.CHIP,
+        graphAL: [['6', 0, 0], ['7', 0, 0]],
+        f: lib.OR,
+      },
+      '6': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.OUTPUT,
+        graphAL: [],
+        f: lib.ID,
+      },
+      '7': {
+        chipId: null,
+        label: '',
+        type: NODE_TYPE.OUTPUT,
+        graphAL: [],
+        f: lib.ID,
+      },
+    },
+  },
+}
+
 export const DEFAULT_CHIPS = {
-  1: {
+  [CHIP_IDS.ID]: {
     name: 'ID',
     description: 'Outputs the same value as the input.',
     functionality: lib.ID,
@@ -21,9 +342,10 @@ export const DEFAULT_CHIPS = {
     ],
     inputs: ['in'],
     outputs: ['out'],
-    id: 1,
+    circuitBoard: CHIP_CIRCUIT_BOARDS.ID,
+    id: CHIP_IDS.ID,
   },
-  2: {
+  [CHIP_IDS.NAND]: {
     name: 'NAND',
     description: 'Outputs true unless both inputs are true',
     functionality: lib.NAND,
@@ -36,9 +358,10 @@ export const DEFAULT_CHIPS = {
     ],
     inputs: ['a', 'b'],
     outputs: ['out'],
-    id: 2,
+    circuitBoard: CHIP_CIRCUIT_BOARDS.NAND,
+    id: CHIP_IDS.NAND,
   },
-  3: {
+  [CHIP_IDS.NOT]: {
     name: 'NOT',
     description: 'Outputs true if the input it false and false if the input is true.',
     functionality: lib.NOT,
@@ -49,9 +372,10 @@ export const DEFAULT_CHIPS = {
     ],
     inputs: ['in'],
     outputs: ['out'],
-    id: 3,
+    circuitBoard: CHIP_CIRCUIT_BOARDS.NOT,
+    id: CHIP_IDS.NOT,
   },
-  4: {
+  [CHIP_IDS.AND]: {
     name: 'AND',
     description: 'Outputs true if both inputs are true.',
     functionality: lib.AND,
@@ -64,9 +388,10 @@ export const DEFAULT_CHIPS = {
     ],
     inputs: ['a', 'b'],
     outputs: ['out'],
-    id: 4,
+    circuitBoard: CHIP_CIRCUIT_BOARDS.AND,
+    id: CHIP_IDS.AND,
   },
-  5: {
+  [CHIP_IDS.OR]: {
     name: 'OR',
     description: 'Outputs true if either input is true.',
     functionality: lib.OR,
@@ -79,9 +404,10 @@ export const DEFAULT_CHIPS = {
     ],
     inputs: ['a', 'b'],
     outputs: ['out'],
-    id: 5,
+    circuitBoard: CHIP_CIRCUIT_BOARDS.OR,
+    id: CHIP_IDS.OR,
   },
-  6: {
+  [CHIP_IDS.MUX]: {
     name: 'MUX',
     description: 'Outputs the value of a if sel is false, otherwise it outputs the value of b.',
     functionality: lib.MUX,
@@ -98,9 +424,10 @@ export const DEFAULT_CHIPS = {
     ],
     inputs: ['a', 'b', 'sel'],
     outputs: ['out'],
-    id: 6,
+    circuitBoard: CHIP_CIRCUIT_BOARDS.MUX,
+    id: CHIP_IDS.MUX,
   },
-  7: {
+  [CHIP_IDS.DMUX]: {
     name: 'DMUX',
     description: 'Output a equals the input value if sel is false, otherwise output b equals the input value.',
     functionality: lib.DMUX,
@@ -113,6 +440,17 @@ export const DEFAULT_CHIPS = {
     ],
     inputs: ['in', 'sel'],
     outputs: ['a', 'b'],
-    id: 7,
+    circuitBoard: CHIP_CIRCUIT_BOARDS.DMUX,
+    id: CHIP_IDS.DMUX,
+  },
+  [CHIP_IDS.DEMO_1]: {
+    name: 'DEMO 1',
+    description: 'Demo circuit board',
+    functionality: () => false,
+    truthTable: [],
+    inputs: ['a', 'b', 'c'],
+    outputs: ['out'],
+    circuitBoard: CHIP_CIRCUIT_BOARDS.DEMO_1,
+    id: CHIP_IDS.DEMO_1,
   },
 }
--- packages/ui-react/src/modules/CircuitBoard/constants.js
+++ packages/ui-react/src/modules/CircuitBoard/constants.js
@@ -1,4 +1,4 @@
-import {lib} from "@nr6/nand2tetris-logic-gates";
+import { CHIP_CIRCUIT_BOARDS } from '../Chip/constants'
 
 export const SVG_WIDTH = 1280
 export const SVG_HEIGHT = 220
@@ -16,65 +16,4 @@ export const DEFAULT_BLANK_NODE = {
   f: () => null,
 }
 
-export const DEFAULT_CIRCUIT_BOARD = {
-  start: ['0', '1', '3'],
-  end: ['6', '7'],
-  nodes: {
-    '0': {
-      chipId: null,
-      label: '',
-      type: NODE_TYPE.INPUT,
-      graphAL: [['2', 0, 0]],
-      f: lib.VALUE (true),
-    },
-    '1': {
-      chipId: null,
-      label: '',
-      type: NODE_TYPE.INPUT,
-      graphAL: [['2', 0, 1]],
-      f: lib.VALUE (false),
-    },
-    '2': {
-      chipId: 4,
-      label: 'AND',
-      type: NODE_TYPE.CHIP,
-      graphAL: [['5', 0, 0]],
-      f: lib.AND,
-    },
-    '3': {
-      chipId: null,
-      label: '',
-      type: NODE_TYPE.INPUT,
-      graphAL: [['4', 0, 0]],
-      f: lib.VALUE (true),
-    },
-    '4': {
-      chipId: 3,
-      label: 'NOT',
-      type: NODE_TYPE.CHIP,
-      graphAL: [['5', 0, 1]],
-      f: lib.NOT,
-    },
-    '5': {
-      chipId: 5,
-      label: 'OR',
-      type: NODE_TYPE.CHIP,
-      graphAL: [['6', 0, 0], ['7', 0, 0]],
-      f: lib.OR,
-    },
-    '6': {
-      chipId: null,
-      label: '',
-      type: NODE_TYPE.OUTPUT,
-      graphAL: [],
-      f: lib.ID,
-    },
-    '7': {
-      chipId: null,
-      label: '',
-      type: NODE_TYPE.OUTPUT,
-      graphAL: [],
-      f: lib.ID,
-    },
-  },
-}
+export const DEFAULT_CIRCUIT_BOARD = CHIP_CIRCUIT_BOARDS.DEMO_1
